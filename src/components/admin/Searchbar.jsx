e8bc7f4805e705667486df399a998f29a46f601c
bertinmick69 <bertinmick@yahoo.fr>
2026-02-16 10:23:27 +0100

    update-comm

--- src/components/admin/Searchbar.jsx
+++ src/components/admin/Searchbar.jsx
@@ -6,10 +6,6 @@ const searchbar = () => {
       <div className="absolute left-4 text-slate-400">
         <Search size={20} />
       </div>
-      {/* <input
-        className="ease mt-10 mb-10 h-13 w-full rounded-xl border border-slate-200 bg-gray-300 py-2 pr-28 pl-3 text-sm text-slate-700 shadow-sm transition duration-300 placeholder:text-slate-400 hover:border-slate-300 focus:border-slate-400 focus:shadow focus:outline-none"
-        placeholder="recherche un film ou un réalisateur"
-      /> */}
       <input
         className="h-14 w-full rounded-2xl border border-transparent bg-gray-100 py-2 pr-4 pl-12 text-sm text-slate-700 shadow-sm transition-all duration-300 placeholder:text-slate-400 hover:bg-gray-200 focus:border-slate-200 focus:bg-white focus:outline-none"
         placeholder="Rechercher un film ou un réalisateur"
